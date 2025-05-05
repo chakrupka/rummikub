9f21308802c818386905df0bdae11a78b430aa40
Cha Krupka <chakrupka238@gmail.com>
2025-05-05 17:33:10 -0400

    updates

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
 from flask import Flask, request
-from solver import optimal_play
 from flask_cors import CORS
 import collections
+from game_v3 import optimal_play
+
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=("GET", "POST"))
 def api():
     if request.method == "GET":
         return "<p>Rummikub Optimal Play Solver</p>"
-    data = request.get_json()
-    board_tiles = collections.Counter(tiles_to_tuples(data["board"]))
-    rack_tiles = collections.Counter(tiles_to_tuples(data["rack"]))
 
-    best_play, played_tiles = optimal_play(board_tiles, rack_tiles)
+    data = request.get_json()
+    board_sets = collections.Counter(format_request(board=data["board"]))
+    rack_tiles = collections.Counter(format_request(rack=data["rack"]))
+
+    best_play, played_tiles = optimal_play(board_sets, rack_tiles)
+
     if not played_tiles:
         return {"best_play": [], "from_rack": []}
+
     formatted_sets, formatted_tiles = format_response(best_play, played_tiles)
     return {"best_play": formatted_sets, "from_rack": formatted_tiles}
 
 
-def tiles_to_tuples(tiles):
-    out = []
-    for color, num in tiles:
-        if num == 0:
-            out.append("J")
-        else:
-            out.append((color, num))
-    return out
+def format_request(board=None, rack=None):
+    if board:
+        tile_sets = []
+        for tile_set in board:
+            formatted_set = [
+                ("J") if num == "J" else (color, int(num)) for color, num in tile_set
+            ]
+            tile_sets.append(tuple(formatted_set))
+        return tile_sets
+
+    if rack:
+        return [("J") if num == "J" else (color, int(num)) for color, num in rack]
 
 
 def format_response(play, tiles):
@@ -37,17 +45,17 @@ def format_response(play, tiles):
     for set_tup in play:
         set_array = []
         for tile in set_tup:
-            if tile != "J":
-                set_array.append([tile[0], tile[1]])
+            if tile == "J":
+                set_array.append(["K", "J"])
             else:
-                set_array.append(["J", 0])
+                set_array.append([tile[0], f"{tile[1]}"])
         formatted_play.append(set_array)
 
     formatted_tiles = []
     for tile in tiles:
-        if tile != "J":
-            formatted_tiles.append([tile[0], tile[1]])
+        if tile == "J":
+            formatted_tiles.append(["K", "J"])
         else:
-            formatted_tiles.append(["J", 0])
+            formatted_tiles.append([tile[0], f"{tile[1]}"])
 
     return formatted_play, formatted_tiles
